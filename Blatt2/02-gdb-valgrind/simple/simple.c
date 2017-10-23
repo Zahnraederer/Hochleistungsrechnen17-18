2be06071c8a47eb1820aa1abce16f2946b30eea2
Kai <k.schmees@googlemail.com>
2017-10-23 16:09:17 +0200

    Präsenztermin1 nach Übung

--- Blatt2/02-gdb-valgrind/simple/simple.c
+++ Blatt2/02-gdb-valgrind/simple/simple.c
@@ -6,33 +6,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *
-mistake1 ()
+int* mistake1 ()
 {
-  int buf[] = { 1, 1, 2, 3, 4, 5 };
+  //int buf[]	 = { 1, 1, 2, 3, 4, 5 };
+  int *buf = malloc (sizeof (int) * 6);
+  buf[0] =  1;
+  buf[1] =  1;
+  buf[2] =  2;
+  buf[3] =  3;
+  buf[4] =  4;
+  buf[5] =  5;
+
+	//TODO array füllen
+
   return buf;
 }
 
-int *
-mistake2 ()
+int* mistake2 ()
 {
-  int *buf = malloc (sizeof (char) * 4);
-  buf[2] = 2;
+  int* buf = malloc (sizeof (char) * 4);
+  buf[1] = 2;
   return buf;
 }
 
-int *
-mistake3 ()
+int* mistake3 ()
 {
   /* In dieser Funktion darf kein Speicher direkt allokiert werden. */
-  int mistake2_ = 0;
-  int *buf = (int *) &mistake2;
+  int mistake2 = 0;
+  int* buf = &mistake2;
   buf[0] = 3;
+  printf ("%d\n", buf[0]);
   return buf;
 }
 
-int *
-mistake4 ()
+int* mistake4 ()
 {
   int *buf = malloc (sizeof (char) * 4);
   buf[4] = 4;
@@ -40,8 +47,7 @@ mistake4 ()
   return buf;
 }
 
-int
-main (void)
+int main (void)
 {
   /* Modifizieren Sie die folgende Zeile nicht */
   int *p[4] = { &mistake1 ()[1], &mistake2 ()[1], mistake3 (), mistake4 () };
